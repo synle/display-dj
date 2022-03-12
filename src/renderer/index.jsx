0fa6262c71601ed6548ab339baeab176c935e9cc
Sy Le <lenguyensy@gmail.com>
2022-03-11 19:51:17 -0800

    added the skeleton

--- src/renderer/index.jsx
+++ src/renderer/index.jsx
@@ -1,23 +1,43 @@
 import ReactDOM from 'react-dom';
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import ApiUtils from './utils/ApiUtils';
 
-// TODO
-ApiUtils.getMonitors().then(console.log);
-ApiUtils.toggleDarkMode().then(console.log);
-ApiUtils.toggleDarkMode(true).then(console.log);
-ApiUtils.updateMonitor({
-  id: '\\\\?\\DISPLAY#VSCB73A#5&21f33940&0&UID4352#{e6f07b5f-ee97-4a90-b076-33f57bf4eaa7}',
-  brightness: 100,
-});
+// // TODO
+// ApiUtils.getMonitors().then(console.log);
+// ApiUtils.toggleDarkMode().then(console.log);
+// ApiUtils.toggleDarkMode(true).then(console.log);
+// ApiUtils.updateMonitor({
+//   id: '\\\\?\\DISPLAY#VSCB73A#5&21f33940&0&UID4352#{e6f07b5f-ee97-4a90-b076-33f57bf4eaa7}',
+//   brightness: 100,
+// });
 
-ApiUtils.updateMonitor({
-  id: '\\\\?\\DISPLAY#VSCB73A#5&23c70c64&0&UID257#{e6f07b5f-ee97-4a90-b076-33f57bf4eaa7}',
-  name: 'monitor #2222',
-});
+// ApiUtils.updateMonitor({
+//   id: '\\\\?\\DISPLAY#VSCB73A#5&23c70c64&0&UID257#{e6f07b5f-ee97-4a90-b076-33f57bf4eaa7}',
+//   name: 'monitor #2222',
+// });
 
 // react
-const Home = () => {
-  return <div>React Application</div>;
+function Home(props) {
+  return <>
+    <header><h1>Display DJ</h1></header>
+    <DarkModeSettingForm />
+    <MonitorBrightnessSettingForm />
+  </>;
 };
+
+function DarkModeSettingForm(props){
+  return <>DarkModeSettingForm</>
+}
+
+function MonitorBrightnessSettingForm(props){
+
+
+  return <>MonitorBrightnessSettingForm</>
+}
+
+function MonitorBrightnessSetting(props){
+  return <>MonitorBrightnessSetting</>
+}
+
+
 ReactDOM.render(<Home />, document.querySelector('#root'));
