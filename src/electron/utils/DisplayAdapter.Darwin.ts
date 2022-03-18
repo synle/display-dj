ea66ef5401586d40f5cda01e5601ff880193009d
Sy Le <lenguyensY@gmail.com>
2022-03-18 16:53:17 -0700

    playground for mac functions

--- src/electron/utils/DisplayAdapter.Darwin.ts
+++ src/electron/utils/DisplayAdapter.Darwin.ts
@@ -1,23 +1,45 @@
-// TODO: TBD and to be implemented
+import { exec } from 'child_process';
 import { DISPLAY_TYPE } from 'src/constants';
 import { DisplayAdapter } from 'src/types.d';
 
-const DisplayUtils : DisplayAdapter= {
-  getMonitorList: async() => {
-    return ['Laptop'];
+const DisplayUtils: DisplayAdapter = {
+  getMonitorList: async () => {
+    return new Promise((resolve, reject) => {
+      const shellToRun = `ddcctl`;
+      exec(shellToRun, (error, stdout, stderr) => {
+        const monitors = (stdout || '')
+          .split('\n')
+          .filter((line) => line.indexOf('D:') === 0)
+          .map((line, idx) => line.replace('D:', '').trim());
+        resolve(monitors);
+      });
+    });
   },
   getMonitorType: async (idToUse: string) => {
-    return DISPLAY_TYPE.LAPTOP;
+    // TODO: to be implemented
+    return DISPLAY_TYPE.EXTERNAL;
   },
   getMonitorBrightness: async (idToUse: string) => {
-    return 50;
+    // TODO: to be implemented
+    return 0;
   },
   updateMonitorBrightness: async (monitorId: string, newBrightness: number) => {
+    return new Promise((resolve, reject) => {
+      const shellToRun = `ddcctl -d ${monitorId} -b ${newBrightness}`;
+      exec(shellToRun, (error, stdout, stderr) => {
+        if (error) {
+          return reject(stderr);
+        }
+        resolve();
+      });
+    });
   },
   getDarkMode: async () => {
+    // TODO: to be implemented
     return true;
   },
   updateDarkMode: async (isDarkModeOn: boolean) => {
+    // TODO: to be implemented
   },
 };
 
--- src/electron/utils/ShellUtils.ts
+++ src/electron/utils/ShellUtils.ts
@@ -1,4 +1,4 @@
-import { spawn } from 'child_process';
+import { spawn, exec } from 'child_process';
 
 export function executePowershell(shellToRun: string, delay = 100): Promise<string> {
   return new Promise((resolve, reject) => {
