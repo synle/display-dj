6cf9a5c9cd601e7424c302a656dd44f191058de8
Sy Le <lenguyensy@gmail.com>
2022-03-18 10:36:26 -0700

    added global typedef for the ui

--- src/index.d.ts
+++ src/index.d.ts
@@ -3,3 +3,27 @@ declare module '@hensm/ddcci' {
   export function getBrightness(id: string): Promise<number>;
   export function setBrightness(id: string, brightness: number): Promise<void>;
 }
+
+
+// for the UI
+declare module "\*.svg" {
+  import React = require("react");
+  export const ReactComponent: React.SFC<React.SVGProps<SVGSVGElement>>;
+  const src: string;
+  export default src;
+}
+
+declare module "\*.jpg" {
+  const content: string;
+  export default content;
+}
+
+declare module "\*.png" {
+  const content: string;
+  export default content;
+}
+
+declare module "\*.json" {
+  const content: string;
+  export default content;
+}
