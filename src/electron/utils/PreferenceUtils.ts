85061e2580fd30bcf3ed2ff0f673a3e31c132afb
Sy Le <lenguyensy@gmail.com>
2022-03-19 09:50:13 -0700

    now properly control the syncing based on undefined of value

--- src/electron/utils/PreferenceUtils.ts
+++ src/electron/utils/PreferenceUtils.ts
@@ -9,14 +9,29 @@ const DEFAULT_PREFERENCES: Preference = {
 const PreferenceUtils = {
   get: async () => {
     let preference: Preference = DEFAULT_PREFERENCES;
+    let shouldSync = false;
 
-    try {
-      preference = StorageUtils.readJSON(PREFERENCE_FILE_PATH, DEFAULT_PREFERENCES);
-    } catch (err) {}
+    preference = StorageUtils.readJSON(PREFERENCE_FILE_PATH);
+
+    if(preference === undefined){
+      preference = DEFAULT_PREFERENCES;
+      shouldSync = true; // sync the default value
+    }
 
     // merging config keys
     for(const prefKey of Object.keys(DEFAULT_PREFERENCES)){
+      //@ts-ignore
+      if(preference[prefKey] === undefined){
+        shouldSync = true;// when a key is missing, then also sync it up
+      }
+
+      // @ts-ignore
       preference[prefKey] = preference[prefKey] || DEFAULT_PREFERENCES[prefKey];
+    }
+
+    if(shouldSync){
+      // essentially we want to sync if the keys mismatched
+      StorageUtils.writeJSON(PREFERENCE_FILE_PATH, preference);
     }
 
     return preference;
