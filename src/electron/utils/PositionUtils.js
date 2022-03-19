45fe700230aef67983bccce4f141491bd07d5cad
Sy Le <lenguyensy@gmail.com>
2022-03-19 15:47:06 -0700

    break up x placement and y placement

--- src/electron/utils/PositionUtils.js
+++ src/electron/utils/PositionUtils.js
@@ -4,37 +4,23 @@ const APP_WIDTH = 300;
 const APP_HEIGHT_MIN = 50;
 
 const PositionUtils = {
-  updateTrayPosition: async (contentHeight, tray, mainWindow) => {
-    if (!tray) {
-      throw `tray and mainWindow not ready yet`;
-    }
-
+  updateTrayPosition: async (mainWindow, contentHeight) => {
     const width = APP_WIDTH;
     const height = Math.max(contentHeight, APP_HEIGHT_MIN);
 
-    const trayBound = tray.getBounds();
-
-    var mainScreen = screen.getPrimaryDisplay();
+    const mainScreen = screen.getPrimaryDisplay();
     const mainScreenSize = mainScreen.size;
-    let x = trayBound.x;
-    let y = trayBound.y;
 
-    const xOffset = 50;
+    let {x,y} = mainWindow.getBounds();
+
     const yOffset = 0;
 
     if (y > mainScreenSize.height / 2) {
       // bottom
-      y = Math.floor(trayBound.y - height - yOffset);
+      y = Math.floor(y - height - yOffset);
     } else {
       // top
-      y = Math.floor(trayBound.y + yOffset);
-    }
-    if (x > mainScreenSize.width / 2) {
-      // right
-      x = Math.floor(trayBound.x - width + xOffset);
-    } else {
-      // left
-      x = Math.floor(trayBound.x + xOffset);
+      y = Math.floor(y + yOffset);
     }
 
     // TODO: need to adjust for left and right vertical taskbar
