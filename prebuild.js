a07bc8ce17abb93e137b375dde15fdd041223dcc
Sy Le <lenguyensY@gmail.com>
2022-03-18 21:02:24 -0700

    fixed build for mac

--- prebuild.js
+++ prebuild.js
@@ -7,9 +7,6 @@ const dest = `src/electron/utils/DisplayAdapter.ts`
 switch(process.platform){
   case 'win32':
   source = `src/electron/utils/DisplayAdapter.Win32.ts`
-  exec(`npm install @hensm/ddcci`, (error) => {
-    console.log('>> installed @hensm/ddcci', error)
-  });
   break;
   case 'darwin':
   source = `src/electron/utils/DisplayAdapter.Darwin.ts`
