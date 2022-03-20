7d5b91404db22f1edc230fcd091a5ae860f8d7e8
Sy Le <lenguyensy@gmail.com>
2022-03-20 16:09:10 -0700

    disabled updating brightness when it's saving...

--- src/renderer/hooks.ts
+++ src/renderer/hooks.ts
@@ -5,7 +5,7 @@ import MonitorSvg from 'src/renderer/svg/monitor.svg';
 import LaptopSvg from 'src/renderer/svg/laptop.svg';
 import ToggleSvg from 'src/renderer/svg/toggle.svg';
 import './index.scss';
-import { Monitor, SingleMonitorUpdateInput } from 'src/types.d';
+import { Monitor, SingleMonitorUpdateInput, UIAppState } from 'src/types.d';
 
 // react query store
 export const QUERY_KEY_CONFIGS = 'configs';
@@ -15,9 +15,9 @@ export const QUERY_KEY_APP_STATE = 'appState';
 export const QUERY_KEY_PREFERENCE = 'preferences';
 
 // app state (not used anymore, but left as is if we want to add new global state to the app)
-type AppState = {};
-
-let _appState: AppState = {};
+let _appState: UIAppState = {
+  isUpdatingBrightness: false
+};
 
 export function useAppState() {
   return useQuery(QUERY_KEY_APP_STATE, () => _appState);
@@ -26,8 +26,8 @@ export function useAppState() {
 export function useUpdateAppState() {
   const queryClient = useQueryClient();
 
-  return useMutation<void, void, AppState>(
-    async (newAppState: AppState) => {
+  return useMutation<void, void, UIAppState>(
+    async (newAppState: UIAppState) => {
       _appState = { ..._appState, ...newAppState };
     },
     {
