942f5567e3fc8400bf771795f15fb891841c726a
Sy Le <lenguyensy@gmail.com>
2022-03-27 20:27:52 -0700

    Create a foundation for sounds utils based on env

--- prebuild.js
+++ prebuild.js
@@ -2,21 +2,33 @@ const fs = require('fs');
 const { exec } = require('child_process');
 
 const DEST_IMPL_DISPLAY_UTILS = `src/main/utils/DisplayAdapter.ts`;
+const DEST_IMPL_SOUND_UTILS = `src/main/utils/SoundUtils.ts`;
+
 let packages = [];
 let files = [];
 
 switch (process.platform) {
   case 'win32':
+    packages = `
+      @hensm/ddcci
+      electron-winstaller
+    `;
     files.push([`src/main/utils/DisplayAdapter.Win32.ts`, DEST_IMPL_DISPLAY_UTILS]);
-    packages = ['@hensm/ddcci', 'electron-winstaller'];
+    files.push([`src/main/utils/SoundUtils.Win32.ts`, DEST_IMPL_SOUND_UTILS]);
     break;
   case 'darwin':
+    packages = `
+      dark-mode
+      loudness
+      electron-installer-dmg
+    `;
     files.push([`src/main/utils/DisplayAdapter.Darwin.ts`, DEST_IMPL_DISPLAY_UTILS]);
-    packages = ['dark-mode', 'electron-installer-dmg'];
+    files.push([`src/main/utils/SoundUtils.Darwin.ts`, DEST_IMPL_SOUND_UTILS]);
     break;
 }
 
 // install extra dependencies
+packages = packages.split(/[ \n]+/).map( s=> s.trim()).filter(s => s);
 exec(`npm install ${packages.join(' ')}`);
 
 // copy over the impls
---
+++ src/main/utils/SoundUtils.Win32.ts
@@ -0,0 +1,11 @@
+// TODO: there is a bundling issue with windows
+// to be investigated afterward
+const SoundUtils = {
+  getVolume: async () => {throw 'To be implemented'},
+  setVolume: async (value: number) => {throw 'To be implemented'},
+  getMuted: async () => {throw 'To be implemented'},
+  setMuted: async (muted: boolean) => {throw 'To be implemented'},
+}
+
+export default SoundUtils;
+
