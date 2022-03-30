e6dddce7a8c4a12100502c8f2df5af9dfcb453be
Sy Le <lenguyensy@gmail.com>
2022-03-30 16:58:06 -0700

    Minor clean up of paths for prebuild and dev envs (#30)
    * change the style to make the path the same
    
    * cleaned up paths
    
    * CI / CD - Prettier Automatic Commit
    
    Co-authored-by: synle <synle@users.noreply.github.com>

--- src/main/utils/SoundUtils.Win32.ts
+++ src/main/utils/SoundUtils.Win32.ts
@@ -1,15 +1,11 @@
 import { executeBash } from 'src/main/utils/ShellUtils';
 import path from 'path';
 
-const binaryPath = process.env.APP_MODE === 'dev'
-  ? path.join(process.cwd(), `src/binaries/win32_volume_helper.exe`)
-  : path.join(process['resourcesPath'], `win32_volume_helper.exe`);
-
-
+const _getVolumeHelperBinary = async () => path.join(process['resourcesPath'], `win32_volume_helper.exe`);
 
 const SoundUtils = {
   getVolume: async() => {
-    const splits = (await executeBash(binaryPath)).split(/[ ]+/).map( s=> s.trim()).filter(s => s);
+    const splits = (await executeBash(await _getVolumeHelperBinary())).split(/[ ]+/).map( s=> s.trim()).filter(s => s);
     const value: number = parseInt(splits[0]);
     const muted: boolean = splits[1] === '1';
 
@@ -22,8 +18,8 @@ const SoundUtils = {
       muted
     }
   },
-  setVolume: async (value: number) => await executeBash(`${binaryPath} ${value}`),
-  setMuted: async (muted: boolean) => await executeBash(`${binaryPath} ${muted ? 'mute' : 'unmute'}`),
+  setVolume: async (value: number) => await executeBash(`${await _getVolumeHelperBinary()} ${value}`),
+  setMuted: async (muted: boolean) => await executeBash(`${await _getVolumeHelperBinary()} ${muted ? 'mute' : 'unmute'}`),
 }
 
 export default SoundUtils;
