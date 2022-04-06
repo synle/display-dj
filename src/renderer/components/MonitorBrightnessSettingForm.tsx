cfc11340b1ed462cdecd6c4e4cea853477a85364
synle <synle@users.noreply.github.com>
2022-04-06 03:02:53 +0000

    CI / CD - Prettier Automatic Commit

--- src/renderer/components/MonitorBrightnessSettingForm.tsx
+++ src/renderer/components/MonitorBrightnessSettingForm.tsx
@@ -1,6 +1,4 @@
-import ReactDOM from 'react-dom';
-import { QueryClient, QueryClientProvider } from 'react-query';
-import React, { useEffect, useMemo, useState } from 'react';
+import { MonitorBrightnessSetting } from 'src/renderer/components/MonitorBrightnessSetting';
 import { LAPTOP_BUILT_IN_DISPLAY_ID } from 'src/constants';
 import MonitorSvg from 'src/renderer/svg/monitor.svg';
 import LaptopSvg from 'src/renderer/svg/laptop.svg';
@@ -20,20 +18,11 @@ import {
   QUERY_KEY_CONFIGS,
   QUERY_KEY_APP_STATE,
 } from 'src/renderer/hooks';
-import { debounce } from 'src/renderer/utils/CommonUtils';
-import { Header } from 'src/renderer/components/Header';
-import { ToggleAllDisplay } from 'src/renderer/components/ToggleAllDisplay';
-import { DarkModeSettingForm } from 'src/renderer/components/DarkModeSettingForm';
-import { MonitorBrightnessSetting } from 'src/renderer/components/MonitorBrightnessSetting';
-import { AllMonitorBrightnessSetting } from 'src/renderer/components/AllMonitorBrightnessSetting';
-import { MonitorNameInput } from 'src/renderer/components/MonitorNameInput';
-import { VolumeSetting } from 'src/renderer/components/VolumeSetting';
-import { VolumeIcon } from 'src/renderer/components/VolumeIcon';
-import { Slider } from 'src/renderer/components/Slider';
 
 type MonitorBrightnessSettingFormProps = {
   monitors: Monitor[];
 };
+
 export function MonitorBrightnessSettingForm(props: MonitorBrightnessSettingFormProps) {
   const { monitors } = props;
   return (
