faf1c4d8a8b31513ec7df9f6fb613a31be63ef76
Sy Le <lenguyensy@gmail.com>
2022-04-06 19:58:45 -0700

    added brightness notification

--- src/main/utils/NotificationUtils.ts
+++ src/main/utils/NotificationUtils.ts
@@ -5,7 +5,15 @@ let lastNotification : Notification | undefined;
 export function showNotification (body: string, title: string  = 'display-dj') {
   if(lastNotification){
     lastNotification.close();
+    lastNotification = undefined;
   }
   lastNotification = new Notification({ title, body});
   lastNotification.show()
+
+  setTimeout(() => {
+    if(lastNotification){
+      lastNotification.close();
+      lastNotification = undefined;
+    }
+  }, 3000);
 }
