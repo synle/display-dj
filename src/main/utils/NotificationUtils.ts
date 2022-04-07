e5e13027bc1f1880e6187cdf93ceab0d85596a81
Sy Le <lenguyensy@gmail.com>
2022-04-06 20:21:05 -0700

    Notification enhancement (auto-dismiss and notification for default brightness command) (#54)
    * minor updates to the keyboard configs
    
    * dismiss last known notification
    
    * added brightness notification
    
    * update snapshots
    
    * CI / CD - Prettier Automatic Commit
    
    Co-authored-by: synle <synle@users.noreply.github.com>

--- src/main/utils/NotificationUtils.ts
+++ src/main/utils/NotificationUtils.ts
@@ -1,5 +1,18 @@
 import { Notification } from 'electron';
+let lastNotification : Notification | undefined;
 
 export function showNotification (body: string, title: string  = 'display-dj') {
-  new Notification({ title, body}).show()
+  if(lastNotification){
+    lastNotification.close();
+    lastNotification = undefined;
+  }
+  lastNotification = new Notification({ title, body});
+  lastNotification.show()
+
+  setTimeout(() => {
+    if(lastNotification){
+      lastNotification.close();
+      lastNotification = undefined;
+    }
+  }, 3000);
 }
