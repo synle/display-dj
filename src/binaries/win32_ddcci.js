8875596b61b4943c0117ab19117be2997be48fd1
Sy Le <lenguyensy@gmail.com>
2022-04-25 18:42:36 -0700

    Split up `@hensm/ddcci` into a separate script - Win32 bug can't adjust brightness after unhook and hook the monitor back in (#65)
    * fixed issue with disconnected monitor
    
    * 1.6.0
    
    * CI / CD - Prettier Automatic Commit
    
    Co-authored-by: synle <synle@users.noreply.github.com>

---
+++ src/binaries/win32_ddcci.js
@@ -0,0 +1,66 @@
+// NOTE: refer to this PR: https://github.com/synle/display-dj/pull/65
+// why do we need to keep this as a separate script?
+// the problem is node ddcci doesn't work for some devices
+// when you disconnect and reconnect
+//
+// having it as a separate script will make sure the initiation
+// of the ddcci works properly in windows
+const ddcci = require("@hensm/ddcci");
+
+process.on('message', async function(msg) {
+  const command = msg[0];
+  const targetMonitorId = msg[1];
+  const newBrightness = parseInt(msg[2]);
+
+
+  try{
+    let res;
+    switch(command){
+      case 'setBrightness':
+        res = await _setBrightness(targetMonitorId, newBrightness);
+        break;
+
+      case 'getBrightness':
+        res = await _getBrightness(targetMonitorId);
+        break;
+
+      case 'getMonitorList':
+        res = await _getMonitorList();
+        break;
+
+      default:
+        throw `Not supported command - ${command}`
+        break;
+    }
+    process.send({success: true, data: res});
+  } catch(error){
+    process.send({success: false, error});
+  }
+});
+
+function _setBrightness(targetMonitorId, newBrightness){
+  if(isNaN(newBrightness) || newBrightness < 0 || newBrightness > 100){
+    throw 'newBrightness needs to be a number between 0 and 100'
+  }
+
+  for (const monitorId of ddcci.getMonitorList()) {
+    if(monitorId === targetMonitorId){
+      try{
+        return ddcci.setBrightness(monitorId, newBrightness);
+      } catch(err){
+        throw err;
+      }
+
+    }
+  }
+
+  throw `targetMonitorId (${targetMonitorId}) not found`;
+}
+
+function _getBrightness(targetMonitorId){
+  return ddcci.getBrightness(targetMonitorId);
+}
+
+function _getMonitorList(){
+  return ddcci.getMonitorList();
+}
