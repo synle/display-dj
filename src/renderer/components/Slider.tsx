b40a408f8734776d94ae3eab74f8c2ab0882db1b
Sy Le <lenguyensy@gmail.com>
2022-04-29 14:44:44 -0700

    now make the updates more responsive

--- src/renderer/components/Slider.tsx
+++ src/renderer/components/Slider.tsx
@@ -1,4 +1,4 @@
-import React, { useMemo } from 'react';
+import React, { useMemo, useState, useEffect } from 'react';
 import { debounce } from 'src/renderer/utils/CommonUtils';
 
 type SliderProps = {
@@ -10,9 +10,28 @@ type SliderProps = {
 };
 
 export function Slider(props: SliderProps) {
+  const [tempVal, setTempVal] = useState(0);
+
   const { value, onInput, className, placeholder, disabled } = props;
 
   const debouncedOnInput = useMemo(() => debounce(onInput, 400), []);
+
+  const onChange = (e: React.FormEvent<HTMLInputElement>) => {
+    setTempVal(parseInt((e.target as HTMLInputElement).value) || 0);
+    debouncedOnInput(e);
+  }
+
+  useEffect(() => {
+    let newValue = value;
+    if(newValue === undefined || newValue <= 0){
+      newValue = 0;
+    }
+
+    if(newValue > 100){
+      newValue = 100;
+    }
+    setTempVal(newValue);
+  }, [value])
 
   return (
     <input
@@ -20,8 +39,8 @@ export function Slider(props: SliderProps) {
       min='0'
       max='100'
       step='10'
-      defaultValue={value}
-      onInput={debouncedOnInput}
+      value={tempVal}
+      onInput={onChange}
       className={className}
       placeholder={placeholder}
       disabled={disabled}
