035748171e4769f70513ba40f2518380d099f289
Sy Le <lenguyensy@gmail.com>
2022-05-18 16:18:28 -0700

    update fix

--- src/renderer/components/Header.tsx
+++ src/renderer/components/Header.tsx
@@ -1,6 +1,13 @@
 import Typography from '@mui/material/Typography';
 import { ToggleAllDisplay } from 'src/renderer/components/ToggleAllDisplay';
 import { AppConfig, Preference } from 'src/types.d';
+import {
+  useConfigs,
+  usePreferences,
+  useRefetchConfigs,
+  useRefetchPreferences,
+  useUpdateAppPosition,
+} from 'src/renderer/hooks';
 
 type HeaderProps = {
   configs?: AppConfig;
@@ -10,10 +17,18 @@ type HeaderProps = {
 export function Header(props: HeaderProps) {
   const { configs, preference } = props;
 
+  const refetchConfigs = useRefetchConfigs();
+  const refetchPreferences = useRefetchPreferences();
+
+  const onRefresh = () => {
+    refetchConfigs();
+    refetchPreferences();
+  }
+
   return (
     <header>
-      <Typography variant='h5' className='flexAlignItems'>
-        Display-DJ {configs?.version} {configs?.env !== 'production' ? configs?.env : ''}
+      <Typography variant='h5' className='flexAlignItems' onClick={onRefresh}>
+        <strong>Display-DJ {configs?.version} {configs?.env !== 'production' ? configs?.env : ''}</strong>
       </Typography>
       {!preference ? null : <ToggleAllDisplay preference={preference} />}
     </header>
