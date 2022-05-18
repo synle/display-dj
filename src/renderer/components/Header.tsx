d687ddd7c55168d2f0f6483d7d54a4662c64c895
Sy Le <lenguyensy@gmail.com>
2022-05-18 16:36:56 -0700

    Clean up the logic for updating / refreshing data (#102)
    * update fix
    
    * CI / CD - Prettier Automatic Commit
    
    Co-authored-by: synle <synle@users.noreply.github.com>

--- src/renderer/components/Header.tsx
+++ src/renderer/components/Header.tsx
@@ -1,5 +1,6 @@
 import Typography from '@mui/material/Typography';
 import { ToggleAllDisplay } from 'src/renderer/components/ToggleAllDisplay';
+import { useRefetchConfigs, useRefetchPreferences } from 'src/renderer/hooks';
 import { AppConfig, Preference } from 'src/types.d';
 
 type HeaderProps = {
@@ -10,10 +11,20 @@ type HeaderProps = {
 export function Header(props: HeaderProps) {
   const { configs, preference } = props;
 
+  const refetchConfigs = useRefetchConfigs();
+  const refetchPreferences = useRefetchPreferences();
+
+  const onRefresh = () => {
+    refetchConfigs();
+    refetchPreferences();
+  };
+
   return (
     <header>
-      <Typography variant='h5' className='flexAlignItems'>
-        Display-DJ {configs?.version} {configs?.env !== 'production' ? configs?.env : ''}
+      <Typography variant='h5' className='flexAlignItems' onClick={onRefresh}>
+        <strong>
+          Display-DJ {configs?.version} {configs?.env !== 'production' ? configs?.env : ''}
+        </strong>
       </Typography>
       {!preference ? null : <ToggleAllDisplay preference={preference} />}
     </header>
