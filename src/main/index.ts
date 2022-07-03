0c11458f9820e46c4df92f38de707cc8850e6411
Sy Le <lenguyensy@gmail.com>
2022-07-03 07:54:11 -0700

    Added minimal support for m1 Macs brightness adjustment (#151)
    * install osascript
    
    * added the binary for m1 mac
    
    * added the display adapter
    
    * remove this
    
    * wip
    
    * wip
    
    * m1 macs specific render
    
    * update readme
    
    * CI / CD - Prettier Automatic Commit
    
    Co-authored-by: synle <synle@users.noreply.github.com>

--- src/main/index.ts
+++ src/main/index.ts
@@ -448,6 +448,16 @@ function _sendRefetchEventToFrontEnd(type: 'all' | 'preferences' | 'configs' = '
     mainWindow.webContents.send(eventName, {type});
   }
 }
+
+function _shouldTraceCall( method: string, url: string){
+  if(method.toLowerCase() === 'get'){
+    if(url.includes('/api/configs') || url.includes('/api/preferences')){
+      return false;
+    }
+  }
+  return true;
+}
+
 // This method will be called when Electron has finished
 // initialization and is ready to create browser windows.
 // Some APIs can only be used after this event occurs.
@@ -506,14 +516,17 @@ ipcMain.on('mainAppEvent/fetch', async (event, data) => {
       if (status >= 300 || status < 200) {
         ok = false;
       }
-      console.trace(
-        'Response',
-        status,
-        method,
-        url,
-        body,
-        '\n' + JSON.stringify(responseData, null, 2),
-      );
+
+      if(_shouldTraceCall(method, url)){
+        console.trace(
+          'Response',
+          status,
+          method,
+          url,
+          body,
+          '\n' + JSON.stringify(responseData, null, 2),
+        );
+      }
       event.reply(requestId, {
         ok,
         status,
