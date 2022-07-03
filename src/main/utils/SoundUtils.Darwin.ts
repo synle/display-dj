40625e94191ef166f65dbb41e019a168aa6c8459
Sy Le <lenguyensy@gmail.com>
2022-07-03 09:09:03 -0700

    use raw osascript to get away from using the loudness level - works in m1

--- src/main/utils/SoundUtils.Darwin.ts
+++ src/main/utils/SoundUtils.Darwin.ts
@@ -1,7 +1,24 @@
-import loudness from 'loudness';
-
 const SoundUtils =  {
   getVolume: async () => {
+    try{
+      const stdout = await executeBash(`osascript -e 'get volume settings'`);
+      stdout = stdout.substr(stdout.indexOf(`output volume:`) + `output volume:`.length)
+
+      const volume = parseInt(stdout.substr(0, stdout.indexOf(`,`)));
+
+      if(volume === 0){
+        return {
+          value: 0,
+          muted: true
+        }
+      }
+
+      return {
+          value: volume,
+          muted: false
+        }
+    }catch(err){}
+
     return {
       value: 50,
       muted: true,
