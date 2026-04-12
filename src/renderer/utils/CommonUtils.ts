dd1e264241ac79c50d22344e4be0a2e3b04a411d
Sy Le <lenguyensy@gmail.com>
2026-04-11 18:54:56 -0700

    wip fix

--- src/renderer/utils/CommonUtils.ts
+++
@@ -1,8 +0,0 @@
-export function debounce(cb: (...args: any[]) => any, delay = 300) {
-  let timerId: any;
-
-  return (...inputs: any[]) => {
-    clearTimeout(timerId);
-    timerId = setTimeout(() => cb(...inputs), delay);
-  };
-}
